4dc6b81853913230ac8b2254175e0d384136e1f8
kangnasharma46 <kangnasharma46@gmail.com>
2026-03-19 17:26:15 +0530

    body+router+login+profile+footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,19 @@
+import { Body } from "./components/Body";
+import { Navbar } from "./components/Navbar";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div class="navbar bg-base-100 shadow-sm bg-teal-500">
-      <div class="flex-1">
-        <a class="btn btn-ghost text-xl text-slate-100">Dev Tinder</a>
-      </div>
-      <div class="flex gap-2">
-        <div class="dropdown dropdown-end">
-          <div
-            tabindex="0"
-            role="button"
-            class="btn btn-ghost btn-circle avatar"
-          >
-            <div class="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
-            </div>
-          </div>
-          <ul
-            tabindex="-1"
-            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a class="justify-between">
-                Profile
-                <span class="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element=<Body />>
+            <Route path="/login"></Route>
+            <Route path="/profile"></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
